745a7769d4065ae731c9b2bd6e8e6b6d63b74dcb
Braja Gopal Das <brajad10@gmail.com>
2025-04-14 19:17:50 +0530

    Asura

---
+++ derivatives/richardson.py
@@ -0,0 +1,12 @@
+import math
+from sympy import sympify, lambdify, symbols
+
+def richardson_extrapolation(expr_str, x, r, h):
+    x_sym = symbols('x')
+    expr = sympify(expr_str)
+    f = lambdify(x_sym, expr, 'math')
+
+    def D_r(fn, x, r, h):
+        return (fn(x + r*h) - fn(x - r*h)) / (2*r*h)
+
+    return (D_r(f, x, r, h) - r**2 * D_r(f, x, 1, h)) / (1 - r**2)
